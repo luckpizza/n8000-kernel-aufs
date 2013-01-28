e725b229bc2de26395ee8bd099229cb152314fb5
Lucas Pizzagalli <luckpizza@gmail.com>
2013-01-28 01:23:52 -0300

    full kernel

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 SMP PREEMPT Mon Jan 28 00:06:01 ART 2013"
+#define LINUX_COMPILE_BY "luckpizza"
+#define LINUX_COMPILE_HOST "Lucass-MacBook-Pro.local"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
